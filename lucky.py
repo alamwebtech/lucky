f2d572eef34adf0e1325dc8d2a624a2a3b9d540b
Md Alam <alamwebtech@gmail.com>
2019-01-20 12:14:02 -0500

    checking lucky and unlucky number in python

---
+++ lucky.py
@@ -0,0 +1,8 @@
+for num in range(1,21):
+    if num == 4 or num == 13:
+        print(f"{num}: This is a UNLUCKY number")
+    elif num % 2 == 0:
+        print(f"{num} This is a even number")
+    else:
+        print(f"{num} This is a odd number")
+	
